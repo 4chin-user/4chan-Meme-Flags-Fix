4e3bd9db3227bc32cc182e7e8e0770ca6ee16548
4chin-user <meowmix831@gmail.com>
2024-10-20 10:37:15 -0700

    Update meme-flags.js

--- meme-flags.js
+++ meme-flags.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @name         Swap bfl Classes with flag flag-il
+// @name         Fix Memeflags
+// @license MIT
 // @namespace    http://tampermonkey.net/
 // @version      1.3
-// @description  Replace any bfl bfl-XX classes with flag flag-il on 4chan boards
+// @description  Fix Memeflags on /pol/
 // @author       You
-// @match        *://boards.4chan.org/*
+// @match        *://boards.4chan.org/pol/*
 // @grant        none
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     // Select all elements that have classes starting with "bfl bfl-"
     var elements = document.querySelectorAll('[class*="bfl bfl-"]');
-
+ 
     // Iterate over each element and swap the class
     elements.forEach(function(element) {
         element.classList.forEach(function(cls) {
